3b09ccb8c8babedfb3808e8ac0a49f479db04ddb
kmilol4gos <k4milolagos@gmail.com>
2024-09-12 16:31:03 -0300

    Add initial project files and configurations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			boxShadow: {
-				button: "0px 0px 10px 0px rgba(0, 0, 0, 0.25);",
-			},
-			backgroundImage: {
-				"gradient-quienes-somos":
-					"linear-gradient(180deg, rgba(251, 251, 251, 0.15) 0%, rgba(153, 206, 66, 0.0855) 55.27%, rgba(251, 251, 251, 0.15) 100%)",
-				"gradient-inicio":
-					"linear-gradient(180deg, rgba(251, 251, 251, 0.15) 0%, rgba(30, 136, 229, 0.15) 55.27%, rgba(251, 251, 251, 0.15) 100%);",
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        button: "0px 0px 10px 0px rgba(0, 0, 0, 0.25);",
+        card: "0px 0px 10px 4px rgba(0, 0, 0, 0.09);",
+      },
+      backgroundImage: {
+        "gradient-quienes-somos":
+          "linear-gradient(180deg, rgba(251, 251, 251, 0.15) 0%, rgba(153, 206, 66, 0.0855) 55.27%, rgba(251, 251, 251, 0.15) 100%)",
+        "gradient-inicio":
+          "linear-gradient(180deg, rgba(251, 251, 251, 0.15) 0%, rgba(30, 136, 229, 0.15) 55.27%, rgba(251, 251, 251, 0.15) 100%);",
+      },
+    },
+  },
+  plugins: [],
 };
